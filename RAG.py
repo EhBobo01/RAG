e3a5ef8e93445922f5f09b61d580fe424924f34b
EHii2001 <enochhii0207@gmail.com>
2025-03-05 20:29:00 +0800

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.sidebar.title("Chatbot Configuration")
 api_key = st.sidebar.text_input("Enter API Key", type="password")
 llm = st.sidebar.selectbox("Select Model", ["gemini-1.5-flash", "gemini-1.5-pro"])
-collection = st.sidebar.file_uploader{"Choose a file"}
+collection = st.sidebar.file_uploader("Choose a file")
 
 
 # Chat interface
