7f30b2a1ef6ec28224b8df0927351d26ee4dba42
EHii2001 <enochhii0207@gmail.com>
2025-03-05 20:28:30 +0800

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -3,7 +3,9 @@ import streamlit as st
 # Sidebar options
 st.sidebar.title("Chatbot Configuration")
 api_key = st.sidebar.text_input("Enter API Key", type="password")
-vector_db_option = st.sidebar.selectbox("Select Vector Database", ["FAISS", "Chroma"])
+llm = st.sidebar.selectbox("Select Model", ["gemini-1.5-flash", "gemini-1.5-pro"])
+collection = st.sidebar.file_uploader{"Choose a file"}
+
 
 # Chat interface
 st.title("AI Chatbot with RAG")
